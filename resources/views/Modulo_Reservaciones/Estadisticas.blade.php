66454e8a4c567715928e92eb7eb6568642f9b673
osiel30 <2230412@upv.edu.mx>
2024-11-04 22:34:24 -0600

    Se sube diseño de la plantilla de disponibilidad

--- resources/views/Modulo_Reservaciones/Estadisticas.blade.php
+++ resources/views/Modulo_Reservaciones/Estadisticas.blade.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Dashboard</title>
-  <link rel="stylesheet" href="{{ asset('/css/Modulo_Reservaciones/Dashboard.css') }}">
+  <title>Estadisticas</title>
+  <link rel="stylesheet" href="{{ asset('/css/Modulo_Reservaciones/Estadisticas.css') }}">
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
