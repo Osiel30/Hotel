8a961efdf78a18c21482dced59d4a41b8161d6b1
ElWalleZ <2230153@upv.edu.mx>
2024-11-04 22:25:20 -0600

    Stock Views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Modulo_Reservaciones.Dashboard');
 });
+
+Route::get('/ordStock', function () {
+    return view('Modulo_Reservaciones.ordenesStock');
+});
+
+Route::get('/stock', function () {
+    return view('Modulo_Reservaciones.stock');
+});
